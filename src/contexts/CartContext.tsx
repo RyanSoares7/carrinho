d7b9785e35097b160f15a747d7ff98ef05ee1a91
RyanSoares7 <ryansoareshub@gmail.com>
2023-10-16 10:36:48 -0300

    feat: Configurando a context api

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 
 type CartProps = {
   id: number;
@@ -12,6 +12,21 @@ type CartProps = {
 
 interface CartContextData {
   cart: CartProps[];
+  cartAmount: number;
+}
+
+interface CartProviderProps {
+  children: ReactNode;
 }
 
 export const CartContext = createContext({} as CartContextData);
+
+function CartProvider({ children }: CartProviderProps) {
+  const [cart, setCart] = useState<CartProps[]>([]);
+
+  return (
+    <CartContext.Provider value={{ cart, cartAmount: cart.length }} >{children}</CartContext.Provider>
+  );
+}
+
+export default CartProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { router } from './App.tsx';
 import { RouterProvider } from 'react-router-dom';
-
 import './index.css';
+import CartProvider from './contexts/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <CartProvider>
+    <RouterProvider router={router} />
+  </CartProvider>
 );
